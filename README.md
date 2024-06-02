7511669b72d72d97659892bb97e0766b1abec60b
Pablo Bento Soares <pbs.97@outlook.com>
2024-06-02 20:39:15 -0300

    Organizando codigo e dependencias

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# build_with_ai
- Integração de bot discord com API GEMINI
+# Gemini Chat
+ Bot criado para consumir a api do gemini...
+ 
+## Sobre
+
+ Bot desenvolvido a partir da api disponibilizada pelo discord:
+ [Dashboard API](https://discord.com/developers/applications) / [Documentação API](https://discord.com/developers/docs/intro)
+ 
+## Tecnologias usadas
+
+<div style="display: inline_block"><br>
+  <img align="center" alt="Gemini-Chat-Python" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg">
+  <img align="center" alt="Discord" height="30" width="40" src="https://www.svgrepo.com/show/452188/discord.svg">
+  <img align="center" alt="Gemini-Chat-Python" height="30" width="40" src="https://github.com/devicons/devicon/blob/master/icons/ubuntu/ubuntu-plain.svg">
+</div>
+
+### Dependências globais
+Python3 / NodeJS / pm2
+
+### Criando ambiente virtual
+
+```bash
+python -m venv build_with_ai
+```
+### Iniciando ambiente virtual
+
+```bash
+cd build_with_ai
+LINUX: ./Scripts/activate
+WINDOWS: ./Scripts/activate.bat
+```
+
+### Dependências locais
+
+```bash
+pip install -r dependency.md
+```
+
+### Diretorios necessarios
+
+```bash
+mkdir /build_with_ai/log/
+```
+
+### Iniciar aplicação
+
+```bash
+pm2 start ecosystem.config.js
+```
+
+### logs prodatinha
+
+```bash
+pm2 logs prodatinha
+```
+
+### Monitorar tarefas pm2
+
+```bash
+pm2 monit
+```
+
+### Reiniciar aplicação
+
+```bash
+pm2 restart prodatinha
+```
+
+### Parar aplicação
+
+```bash
+pm2 stop prodatinha
+```
+
+### logs
+
+```bash
+pm2 logs prodatinha
+```
+
+# Melhorias
+
+    Adicionar controle de containers para subir e testar se a aplicação esta funcionando corretamente;
--- src/main.py
+++ src/main.py
@@ -1,36 +1,22 @@
-import os
-import random
-import inspect
-import discord
-import platform
-import util.logger as logger
-import config.config as config
-import google.generativeai as genai
+"""
+Install the Google AI Python SDK
 
-from discord.ext import tasks, commands 
-from discord.ext.commands import Bot  
-from discord.errors import GatewayNotFound
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+import os
+import util.logger as logger
+import google.generativeai as genai
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-logger = logger.setup_logger('gemini_chat', '../log/gemini_chat.log')
-config = config.config.load_config() 
+logger = logger.setup_logger('gemini_chat', '../log/gemini_chat.log') 
 
-bot = Bot
-bot.config = config  
 
-intents = discord.Intents.all()
-intents.presences = False
-intents.messages = True
-intents.guilds = True
-
-bot = Bot(
-    command_prefix=commands.when_mentioned_or(config['prefix']), 
-    intents=intents,
-    help_command=None,
-)
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
@@ -76,50 +62,3 @@ chat_session = model.start_chat(
 
 # response = chat_session.send_message("Teste de modelo")
 # print(response)
-
-async def status_bot(status=None, estado=None) -> None: 
-
-    processo_atual = inspect.currentframe().f_code.co_name 
-
-    try:
-        statuses = ['com outros bots.', 'Paciência!', 'com humanos!']
-        
-        if status is None: 
-            await bot.change_presence(activity=discord.Game(random.choice(statuses))) 
-            logger.info(f'Status de presença do bot modificado aletoariamente.')
-            return
-        
-        if status is not None: await bot.change_presence(activity=discord.Game(str(status))) 
-        if estado is not None: await bot.change_presence(status=estado) 
-    
-    except Exception as exception:
-
-        logger.error(f'{processo_atual} - {exception}')
-
-@bot.event
-async def on_ready():
-   
-    logger.info(f'Conectado com {bot.user.name}!') 
-    logger.info(f'Versão API discord.py: {discord.__version__}')
-    logger.info(f'Versão Python: {platform.python_version()}')
-    logger.info(f'Rodando na plataforma: {platform.system()} {platform.release()} ({os.name})')
-    logger.info('-------------------')
-
-    if config['sync_commands_globally']: 
-        logger.info('Sincronizado com comandos globais...')
-        await bot.tree.sync()
-
-@bot.event
-async def on_close(error):
-    if isinstance(error, discord.errors.GatewayNotFound):
-        logger.warning(f'Gateway not found: {error}')
-    else:
-        logger.error(f'WebSocket closed unexpectedly: {error}')
-
-try:
-
-    bot.run(config['token']) 
-    
-except GatewayNotFound as exeption:
-
-    logger.warnning(f'{exeption}') 
